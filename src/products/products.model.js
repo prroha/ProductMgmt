31271f8ae63c6f7e4edfb31f5872facd72c6d6cc
Prakash <pnlwlusts@gmail.com>
2024-01-08 22:23:01 +0545

    file deleted

--- src/products/products.model.js
+++
@@ -1,9 +0,0 @@
-const Product = {
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  token: [{ type: String }],
-  resetToken: [{ type: String }],
-  name: '',
-}
-
-export default Product
